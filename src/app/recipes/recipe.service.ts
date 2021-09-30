0393579a4f50726a90fda49de64372f44ed3d1dc
Szabi <kriskoszabi@gmail.com>
2021-10-01 00:14:51 +0200

    add ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,10 +7,21 @@ export class RecipeService {
    selectedRecipe = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] =[
-        new Recipe('Test Recipe', 'description',
-        'https://hips.hearstapps.com/delish/assets/17/31/1501791674-delish-chicken-curry-horizontal.jpg'),
-        new Recipe('Test 2 Recipe', 'description 2',
-        'https://hips.hearstapps.com/delish/assets/17/31/1501791674-delish-chicken-curry-horizontal.jpg')
+        new Recipe('Test Recipe',
+                   'description',
+                   'https://hips.hearstapps.com/delish/assets/17/31/1501791674-delish-chicken-curry-horizontal.jpg',
+                   [
+                      new Ingredient('Meat', 2),
+                      new Ingredient('Potato', 10)
+                   ]),
+
+        new Recipe('Test 2 Recipe',
+                   'description 2',
+                   'https://hips.hearstapps.com/delish/assets/17/31/1501791674-delish-chicken-curry-horizontal.jpg',
+                  [
+                     new Ingredient('Meat', 2),
+                     new Ingredient('Salad', 30)
+                  ]),
     ];
 
     getRecipes() {
